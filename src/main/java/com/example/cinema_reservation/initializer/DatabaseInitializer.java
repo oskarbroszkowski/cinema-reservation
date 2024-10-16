616ec16426741b068e75290f5ab456479a115d20
oskarbroszkowski <142011604+oskarbroszkowski@users.noreply.github.com>
2024-10-16 13:06:18 +0200

    security and validation

---
+++ src/main/java/com/example/cinema_reservation/initializer/DatabaseInitializer.java
@@ -0,0 +1,62 @@
+package com.example.cinema_reservation.initializer;
+
+import com.example.cinema_reservation.model.*;
+import com.example.cinema_reservation.repository.CinemaHallRepository;
+import com.example.cinema_reservation.repository.MovieRepository;
+import com.example.cinema_reservation.repository.ScreeningRepository;
+import com.example.cinema_reservation.repository.UserRepository;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class DatabaseInitializer {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final MovieRepository movieRepository;
+    private final CinemaHallRepository cinemaHallRepository;
+    private final ScreeningRepository screeningRepository;
+
+    @Autowired
+    public DatabaseInitializer(UserRepository userRepository, PasswordEncoder passwordEncoder, MovieRepository movieRepository,
+    CinemaHallRepository cinemaHallRepository, ScreeningRepository screeningRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.movieRepository = movieRepository;
+        this.cinemaHallRepository = cinemaHallRepository;
+        this.screeningRepository = screeningRepository;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        ClassPathResource resource = new ClassPathResource("data.json");
+
+        DataFile dataFile = objectMapper.readValue(resource.getFile(), DataFile.class);
+
+        List<User> users = dataFile.getUsers();
+        for (User user : users) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+        }
+        userRepository.saveAll(users);
+
+        List<Movie> movies = dataFile.getMovies();
+        movieRepository.saveAll(movies);
+
+        List<CinemaHall> cinemaHalls = dataFile.getCinemaHalls();
+        cinemaHallRepository.saveAll(cinemaHalls);
+
+        List<Screening> screenings = dataFile.getScreenings();
+        screeningRepository.saveAll(screenings);
+    }
+}
---
+++ src/main/java/com/example/cinema_reservation/security/SecurityConfiguration.java
@@ -0,0 +1,61 @@
+package com.example.cinema_reservation.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/login*", "/register*", "/h2-console/**",
+                                "/homepage*", "/images/**", "/favicon.ico", "/error",
+                                "/logout*", "/register-success", "/screenings*",
+                                "/movie/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.
+                        FrameOptionsConfig::disable))
+                .securityContext((securityContext) -> securityContext
+                .securityContextRepository(new DelegatingSecurityContextRepository(
+                        new RequestAttributeSecurityContextRepository(),
+                        new HttpSessionSecurityContextRepository()
+                )));
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
